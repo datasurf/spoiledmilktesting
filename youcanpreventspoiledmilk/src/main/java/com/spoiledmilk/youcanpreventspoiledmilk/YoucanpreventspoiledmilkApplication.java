e190adb5918fdb070a7d2267cd5190b28f3ef0aa
claudiolespier <clespier@gmail.com>
2022-01-16 21:29:41 -0500

    First Commit!

---
+++ youcanpreventspoiledmilk/src/main/java/com/spoiledmilk/youcanpreventspoiledmilk/YoucanpreventspoiledmilkApplication.java
@@ -0,0 +1,13 @@
+package com.spoiledmilk.youcanpreventspoiledmilk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoucanpreventspoiledmilkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoucanpreventspoiledmilkApplication.class, args);
+	}
+
+}
---
+++ youcanpreventspoiledmilk/src/test/java/com/spoiledmilk/youcanpreventspoiledmilk/YoucanpreventspoiledmilkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spoiledmilk.youcanpreventspoiledmilk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoucanpreventspoiledmilkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
